3e2f10858054818b4db918155d912adb05373344
PC <andrey.rumiantsev95@gmail.com>
2022-08-22 20:24:30 +0300

    add mobx

--- src/screens/quotes/Quotes.tsx
+++ src/screens/quotes/Quotes.tsx
@@ -1,33 +1,35 @@
-import React from "react";
-import {
-  FlatList,
-  ListRenderItemInfo,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextStyle,
-  View,
-  ViewStyle,
-} from "react-native";
+import React, { useEffect } from "react";
+import { FlatList, ListRenderItemInfo, SafeAreaView, StyleSheet, Text, TextStyle, View, ViewStyle } from "react-native";
 import { HeaderComponent } from "../../components/HeaderComponent";
 import { Tabs } from "../../navigation/components/navigationPages";
 import { useIsFocused, useNavigation } from "@react-navigation/native";
-import { useGetTickerQuery } from "../../redux/poloniex.api";
-import { LoadingComponent } from "../../components/LoadingComponent";
 import { IPoloniexModelItem } from "../../models/poloniex.model";
 import { QuotesItemComponent } from "./components/QuotesItemComponent";
 import { Colors } from "../../globalTheme/colors";
 import { QuotesHeaderName } from "./components/QuotesHeaderName";
+import { store } from "../../store/store";
+import { observer } from "mobx-react";
+import { LoadingComponent } from "../../components/LoadingComponent";
+import { windowWidth } from "../../globalTheme/constants";
 
-export const Quotes = () => {
+export const Quotes = observer(() => {
   const navigation = useNavigation();
   const isFocused = useIsFocused();
 
-  const { isLoading, isError, data } = useGetTickerQuery(undefined, {
-    pollingInterval: isFocused ? 5000 : 0,
-  });
+  let intervalId: NodeJS.Timeout;
+  useEffect(() => {
+    store.fetchApiData();
+    if (isFocused) {
+      intervalId = setInterval(() => {
+        store.fetchApiData();
+      }, 5000);
+    }
 
-  const goBackHandler = () => {
+    return () => clearInterval(intervalId);
+
+  }, [isFocused]);
+
+  const goBackHandler = async () => {
     navigation.goBack();
   };
 
@@ -41,7 +43,19 @@ export const Quotes = () => {
     />;
   };
 
-  if (isLoading) {
+  const renderErr = () => {
+    return (
+      <View style={styles.errorView}>
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorDescription}>
+            Something went wrong
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  if (store.responseData.length == 0 && !store.isError) {
     return <LoadingComponent />;
   }
 
@@ -49,29 +63,23 @@ export const Quotes = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <HeaderComponent onPressBack={goBackHandler} title={Tabs.quotes} />
-        <View style={styles.elContainer}>
-          {
-            isError ?
-              <View style={styles.errorContainer}>
-                <Text style={styles.errorDescription}>
-                  Something went wrong
-                </Text>
-              </View> : null
-          }
-          <QuotesHeaderName />
-          <FlatList
-            data={data}
-            renderItem={renderItem}
-            showsVerticalScrollIndicator={false}
-            keyExtractor={item => item.id.toString()}
-            contentContainerStyle={styles.flatListContainer}
-          />
-        </View>
+        {store.isError ? renderErr() :
+          <View style={styles.elContainer}>
+            <QuotesHeaderName />
+            <FlatList
+              data={store.responseData}
+              renderItem={renderItem}
+              showsVerticalScrollIndicator={false}
+              keyExtractor={item => item.id?.toString()}
+              contentContainerStyle={styles.flatListContainer}
+            />
+          </View>
+        }
       </View>
       <View style={styles.empty} />
     </SafeAreaView>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -85,6 +93,11 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.backRed,
     paddingVertical: 16,
     borderRadius: 18,
+  } as ViewStyle,
+  errorView: {
+    paddingHorizontal: 16,
+    width: windowWidth,
+    paddingTop: 8,
   } as ViewStyle,
   errorDescription: {
     color: Colors.red,
